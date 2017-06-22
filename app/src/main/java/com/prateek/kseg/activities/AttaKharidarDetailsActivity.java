ffdee0cc222c9655580c058d4f2e0325c4fcaab5
prateek31291 <prateek.chaudhary@videotap.com>
2017-06-22 10:51:55 +0530

    kseg first complete project push 22 june 17

---
+++ app/src/main/java/com/prateek/kseg/activities/AttaKharidarDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.prateek.kseg.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.prateek.kseg.R;
+
+/**
+ * Created by prateek on 20/6/17.
+ */
+
+public class AttaKharidarDetailsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attakharidardetails);
+    }
+}
